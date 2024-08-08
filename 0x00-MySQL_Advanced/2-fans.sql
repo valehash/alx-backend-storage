b8d39276a8c0c11aef6f0105041fa8b7d545e3b0
vale-hash <newlechim@gmail.com>
2024-08-08 22:07:32 +0100

    added task2

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,3 +1,3 @@
 -- script to CREATE a user tbale it's params
 -- icountry, enumeration of countries: US, CO and TN, never null (= default will be the first element of the enumeration, here US)
-SELECT origin, SUM(fans) as nb_fans from metal_bands GROUP BY origin DESC;
+SELECT origin, SUM(fans) as nb_fans from metal_bands GROUP BY origin ORDER BY nb_fans DESC;
