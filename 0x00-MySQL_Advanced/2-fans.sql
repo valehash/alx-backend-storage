f61d2be054013e2799cf2accf9ce7cbc44d91180
vale-hash <newlechim@gmail.com>
2024-08-09 01:37:25 +0100

    added task3

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,3 +1,4 @@
 -- script to CREATE a user tbale it's params
 -- icountry, enumeration of countries: US, CO and TN, never null (= default will be the first element of the enumeration, here US)
 SELECT origin, SUM(fans) as nb_fans from metal_bands GROUP BY origin ORDER BY nb_fans DESC;
+
---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,1 @@
+SELECT band_name, (IFNULL(split,2020) - formed) AS lifespan FROM metal_bands WHERE style LIKE '%Glam rock%';
