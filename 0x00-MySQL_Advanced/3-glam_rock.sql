fc1b8f7195d666306f02a39c88c4e19e8b102fd3
vale-hash <newlechim@gmail.com>
2024-08-09 01:40:56 +0100

    added task3

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,2 +1,2 @@
 -- gets rock artist that have glam rock as a style
-SELECT band_name, (IFNULL(split,2020) - formed) AS lifespan FROM metal_bands WHERE style LIKE '%Glam rock%';
+SELECT band_name, (IFNULL(split,2020) - formed) AS lifespan FROM metal_bands WHERE style LIKE '%Glam rock%' OR style LIKE '%Glam rock' OR ;style LIKE 'Glam rock%'
