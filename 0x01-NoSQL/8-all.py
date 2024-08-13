de68fe148fdf5ba85ee5aee10955d31b2ff654ae
vale-hash <newlechim@gmail.com>
2024-08-13 13:31:42 +0100

    mongodb + python

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
-"""Scrpt that list all documents in a collection"""
-
-
+"""Script that list all documents in a collection"""
 from typing import List 
 from pymongo.collection import Collection
 
-def list_all(mongocollection: Collection) -> List[dict]:
+def list_all(mongo_collection: Collection) -> List[dict]:
     """
     Args:
         mongocollection: The mongocollection object
     Returns:
         Returns a list of all documents in the collection
     """
-    
-    return list(mongocollection.find())
+    if mongo_collection is None:
+        return []
+
+    return list(mongo_collection.find({}))
 
